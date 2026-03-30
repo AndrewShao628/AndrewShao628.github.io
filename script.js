ec60dd9e589d5d2a870ccfc87c7aa5aa8d1a9736
AndrewShao628 <shaojinghong@gmail.com>
2026-03-30 01:12:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // script.js
+
+// smooth scroll for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
@@ -7,3 +9,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Navbar subtly shrinks when scroll
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.style.padding = '0.7rem 3rem';
+  } else {
+    navbar.style.padding = '1.1rem 3rem';
+  }
+}, { passive: true });
